70b3645dc8f5cadac8a75bd52f19ff2f65f45311
Jakub Korytko <jakub@korytko.me>
2025-09-02 14:57:41 +0200

    Move AUTH_TYPE documentation to the patch

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -11,63 +11,30 @@ const CONST = {
     },
     /** Type of used authentication method returned by the SecureStore mapped to names */
     AUTH_TYPE: {
-      /**
-       * This is purely for backwards compatibility.
-       * Although it is not listed as a return value of the getAuthenticationType() method,
-       * it is still present in the Android code.
-       * @see https://developer.android.com/reference/android/hardware/biometrics/BiometricPrompt.AuthenticationResult#getAuthenticationType()
-       * @see https://developer.android.com/reference/androidx/biometric/BiometricPrompt#AUTHENTICATION_RESULT_TYPE_UNKNOWN()
-       * @platform android
-       */
       UNKNOWN: {
         CODE: AUTH_TYPE.UNKNOWN,
         NAME: "Unknown",
       },
-      /**
-       * Returned when the authentication fails
-       * @platform android
-       * @platform ios
-       */
       NONE: {
         CODE: AUTH_TYPE.NONE,
         NAME: "None",
       },
-      /**
-       * Generic type, not specified whether it was a passcode or pattern.
-       * @platform android
-       * @platform ios
-       */
       CREDENTIALS: {
         CODE: AUTH_TYPE.CREDENTIALS,
         NAME: "Credentials",
       },
-      /**
-       * Generic type, not specified whether it was a face scan or a fingerprint
-       * @platform android
-       */
       BIOMETRICS: {
         CODE: AUTH_TYPE.BIOMETRICS,
         NAME: "Biometrics",
       },
-      /**
-       * FaceID was used to authenticate
-       * @platform ios
-       */
       FACE_ID: {
         CODE: AUTH_TYPE.FACE_ID,
         NAME: "FaceID",
       },
-      /**
-       * TouchID was used to authenticate
-       * @platform ios
-       */
       TOUCH_ID: {
         CODE: AUTH_TYPE.TOUCH_ID,
         NAME: "TouchID",
       },
-      /**
-       * OpticID was used to authenticate (reserved by apple, used on Apple Vision Pro, not iOS)
-       */
       OPTIC_ID: {
         CODE: AUTH_TYPE.OPTIC_ID,
         NAME: "OpticID",
